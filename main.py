9b3eaef1b2df73f32e8c7119ab555bc265486bd9
Yusuf <yusuf.a@demandnxt.com>
2023-07-03 15:21:36 +0530

    Newly Cleaned data
    - Fetched data from DB
    - Cleaned data by filtering personal emails from professional
    - Sorted data by fields
    - Organized columns according to DB column names
    - Exported data back to DB

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+df = pd.read_csv('phone.csv')
+email_list = df['emails'].values.tolist()
+
+
+no_of_emails = []
+for i in email_list:
+    if 'personal' in i:
+        no_of_emails.append(i.count('personal'))
+
+    else:
+        print('None')
+
+count = 0
+pmail = []
+for i,j in enumerate(email_list):
+    b = email_list[i].split(',')
+    for i,j in enumerate(b):
+        if 'personal' in j:
+            pmail.append(b[i-1].split(':')[1])
+            count += 1
+
+count = 0
+k = 0
+
+for i in no_of_emails:
+    email = ''
+    for j in range(0,i):
+        if i > 1:
+            email = email + pmail[k] + ', '
+        else:
+            email = email + pmail[k] 
+        k = k + 1
+    df.loc[df.index[count], 'Personal_Email'] = email
+    count += 1
+
+
+df.to_csv("phone1.csv")
